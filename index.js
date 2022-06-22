e8ca4ecac51ce882a4d3c883d8cb5f4bd762f1fc
Elohor-Grace <omorogunelohor@gmail.com>
2022-06-22 00:11:15 -0700

    first task on intro to java

---
+++ index.js
@@ -0,0 +1,6 @@
+document.getElementById("heading1").innerHTML="Name: Omorogun Elohor Grace";
+document.getElementById("heading2").innerHTML="Height: 5 Feet";
+document.getElementById("heading3").innerHTML="Country: Nigeria";
+
+
+
